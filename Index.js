f0c4d20f93b6be8a93a2219b9877d0a25b8f7b2f
rai369770-ship-it <rai369770@gmail.com>
2026-03-08 06:28:39 +0545

    Update Index.js

--- Index.js
+++ Index.js
@@ -478,10 +478,7 @@ app.get('/', (req, res) => {
         }
     });
 });
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+export default app;
 });
 
 export default app;
